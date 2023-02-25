2721968743dfed4ffd1b64e3565f3619b48bfbef
altairsantos <altairsantosvix@gmail.com>
2023-02-25 18:40:54 -0500

    blockchain basic structure

---
+++ blockchain.go
@@ -0,0 +1,41 @@
+// Project Trendstone
+// Blockchain implentation with proof of work
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp       %d\n", b.timestamp)
+	fmt.Printf("nonce           %d\n", b.nonce)
+	fmt.Printf("previous_hash   %s\n", b.previousHash)
+	fmt.Printf("transactions    %s\n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("TrendStone Blockchain: ")
+}
+
+func main() {
+	b := NewBlock(0, "init hash(Genesis Block)")
+	b.Print()
+}
